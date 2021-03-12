29c8188fd865a7db522095aab432f266fa9e8e4e
Teun Spithoven <74899257+TeunSpithoven@users.noreply.github.com>
2021-03-12 13:07:38 +0100

    Vraag 1 tm 3 af nu bezig met 4

--- Puzels/Form1.Designer.cs
+++ Puzels/Form1.Designer.cs
@@ -37,8 +37,20 @@ namespace Puzels
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBoxTwee = new System.Windows.Forms.TextBox();
+            this.textBoxEen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -131,11 +143,128 @@ namespace Puzels
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(35, 379);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            -1444956740,
+            13989,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(149, 20);
+            this.numericUpDown3.TabIndex = 11;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            -443946297,
+            139,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Vraag 3";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button3.Location = new System.Drawing.Point(35, 405);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 108);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "GO!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(190, 379);
+            this.textBox3.MaxLength = 999999999;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 134);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(381, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Vraag 4";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(539, 135);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(167, 50);
+            this.textBox4.TabIndex = 15;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.button4.Location = new System.Drawing.Point(384, 69);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 116);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "GO!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBoxTwee
+            // 
+            this.textBoxTwee.Location = new System.Drawing.Point(466, 43);
+            this.textBoxTwee.Name = "textBoxTwee";
+            this.textBoxTwee.Size = new System.Drawing.Size(67, 20);
+            this.textBoxTwee.TabIndex = 17;
+            // 
+            // textBoxEen
+            // 
+            this.textBoxEen.Location = new System.Drawing.Point(384, 43);
+            this.textBoxEen.Name = "textBoxEen";
+            this.textBoxEen.Size = new System.Drawing.Size(65, 20);
+            this.textBoxEen.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(446, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = " X";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(539, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 95);
+            this.listBox1.TabIndex = 20;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 567);
+            this.ClientSize = new System.Drawing.Size(745, 541);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxEen);
+            this.Controls.Add(this.textBoxTwee);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
@@ -149,6 +278,7 @@ namespace Puzels
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,6 +294,17 @@ namespace Puzels
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBoxTwee;
+        private System.Windows.Forms.TextBox textBoxEen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Puzels/Form1.cs
+++ Puzels/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
 
 namespace Puzels
@@ -19,25 +15,24 @@ namespace Puzels
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         // ---------------------------------------------------------VRAAG 1------------------------------------------------------
-        
+
         // als er op de knop wordt geklikt:
         private void button1_Click(object sender, EventArgs e)
         {
+            textBox1.Clear();
             // roept functie aan en gebruikt daarbij de input van de numericupdown
             int output = Vraag1(Convert.ToInt32(numericUpDown1.Value));
             textBox1.Text = Convert.ToString(output);
         }
 
-        
         // som van alle veelvouden van 3 en 5 onder het ingegeven getal
         //vb: in = 10 uit= 3+5+6+9 = 23
         public int Vraag1(int input)
         {
-            int sum =0;
+            int sum = 0;
 
             // voor ieder getal vanaf 0 tot input:
             for (int i = 0; i < input; i++)
@@ -63,18 +58,18 @@ namespace Puzels
             int getal1 = 1;
             int getal2 = 2;
             int tussenGetal = 1;
-            
+
             while (tussenGetal <= input)
             {
                 //het volgende getal in de reeks:
                 tussenGetal = getal2 + getal1;
-                
+
                 // als die even is wordt hij opgeteld
                 if (tussenGetal % 2 == 0)
                 {
                     som += tussenGetal;
                 }
-                
+
                 // de getallen voor de berekening schuiven een plaats naar rechts op
                 getal1 = getal2;
                 getal2 = tussenGetal;
@@ -85,9 +80,124 @@ namespace Puzels
 
         private void button2_Click(object sender, EventArgs e)
         {
+            textBox2.Clear();
             // roept functie aan en gebruikt daarbij de input van de numericupdown
             int output = Vraag2(Convert.ToInt32(numericUpDown2.Value));
             textBox2.Text = Convert.ToString(output);
+        }
+
+        // ---------------------------------------------------------VRAAG 3------------------------------------------------------
+
+        public void Vraag3()
+        {
+            // decimalen omdat die heel groot mogen zijn
+            decimal b;
+            decimal a = numericUpDown3.Value;
+            
+            // zolang a groter is dan 1
+            for (b = 2; a > 1; b++)
+                //als het huidige getal een priemgetal is
+                if (a % b == 0)
+                {
+                    var x = 0;
+                    // kijken hoe vaak dat priemgetal voorkomt
+                    while (a % b == 0)
+                    {
+                        a /= b;
+                        x++;
+                    }
+                    // print de priemfactoren en hoe vaak ze voorkomen
+                    textBox3.Text += $"{b} x {x} -- ";
+                }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // zodat je meerdere keren kunt gebruiken maak je de textbox eerst leeg
+            textBox3.Clear();
+            Vraag3();
+        }
+
+        // ---------------------------------------------------------VRAAG 3------------------------------------------------------
+
+        public void Vraag4(int nummerEen, int nummerTwee)
+        {
+            // het product van de twee invoervelden
+            int product = nummerEen * nummerTwee;
+            
+            // zolang dat product niet gespiegelt kan worden:
+            while (SpiegelCheck(product) == false)
+            {
+                nummerEen++;
+                nummerTwee++;
+            }
+            // als die wel gespiegelt kan worden:
+            if (SpiegelCheck(product))
+            {
+                textBox4.Text = product.ToString();
+            }
+            
+        }
+
+        public bool SpiegelCheck(int product)
+        {
+            bool returnBool;
+
+            // deze werkt, als je 512 hebt, krijg je {5, 1, 2}
+            int[] intArray = GetIntList(product);
+
+            int i = 0;
+            while (i < intArray.Length)
+            {
+                listBox1.Items.Add(intArray[i]);
+                i++;
+            }
+
+            int a = 0;
+            int b = intArray.Length -1;
+            int succes = 0;
+            
+            // maximale lengte is 6 dus 3 keer checken van buiten naar binnen
+            while (a <= 3){
+                if (intArray[a] == intArray[b])
+                {
+                    succes++;
+                }
+                a++;
+                b--;
+            }
+
+            // als je 3 keer dezelfe hebt is t goed
+            if (succes == 3)
+            {
+                returnBool = true;
+            }
+            // als dat niet is is het fout
+            else
+            {
+                returnBool = false;
+            }
+
+            return returnBool;
+        }
+        
+        // zet een int om naar een array van losse getallen
+        public int[] GetIntList(int num)
+        {
+            List<int> listOfInts = new List<int>();
+            while (num > 0)
+            {
+                listOfInts.Add(num % 10);
+                num = num / 10;
+            }
+            listOfInts.Reverse();
+            return listOfInts.ToArray();
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            Vraag4(Convert.ToInt32(textBoxEen.Text), Convert.ToInt32(textBoxTwee.Text));
         }
     }
 }
